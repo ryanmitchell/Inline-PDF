10404106cc7e0f9f93988921dd53859cb9240032
Ryan Mitchell <ryan@Ryan.local>
2010-05-10 13:56:16 +0100

    Plenty of updates, the whole thing works quite nicely now

--- Source/InlinePDF.js
+++ Source/InlinePDF.js
@@ -23,7 +23,8 @@ var InlinePDF = new Class({
 		selectedClass: 'selected',
 		showThumbs: true,
 		showBackForward: true,
-		showZoom: true
+		showZoom: true,
+		showDownload: true
 	},
 	
 	// values corresponding to the pdf
@@ -40,10 +41,15 @@ var InlinePDF = new Class({
 		this.setOptions(o);
 	
 		// set up our request object
-		this.request = new Request.JSON({ url:this.options.url, method:'post' }).addEvent('success', this.success.bind(this)).addEvent('failure', this.failure.bind(this));
+		this.request = new Request.JSON({ url:this.options.url, method:'post' })
+		.addEvent('success', this.success.bind(this))
+		.addEvent('failure', this.failure.bind(this));
 	
 		// canvas or not?
-		this.canvas = canvasSupported();
+		this.canvas = this.canvasSupported();
+		
+		// setup
+		this.setup();
 	
 	},
 	
@@ -51,8 +57,106 @@ var InlinePDF = new Class({
 	setup: function(){
 	
 		// create our viewer window etc, canvas with fallback for non-canvas browsers
+		this.viewer = document.id(this.options.el).empty();
 		
-		// note, showThumbs, showBackForward, showZoom
+		// main page viewer
+		this.viewer.adopt(new Element('div').addClass('page'));
+		
+		// show back/foward
+		if (this.options.showBackForward){
+		
+			var self = this;
+			
+			var bf = new Element('div', { html: '<p></p>' }).addClass('nav');
+			
+			var bk = new Element('a', { html: 'Back', href: '#back' })
+			.addEvent('click', function(ev){
+			
+				if (ev) ev.stop();
+			
+				if (self.currentpage > 0){
+					self.changePage(self.currentpage - 1);
+				}
+			
+			});
+			
+			bf.getElement('p').adopt(bk).adopt(new Element('span', {html: ' '}));
+			
+			var nx = new Element('a', { html: 'Forward', href: '#next' })
+			.addEvent('click', function(ev){
+			
+				if (ev) ev.stop();
+			
+				if (self.currentpage < (self.pagecount - 1)){
+					self.changePage(self.currentpage + 1);
+				}
+			
+			});
+			
+			bf.getElement('p').adopt(nx);
+			
+			// adopt back/forward
+			this.viewer.adopt(bf);
+		
+		}
+		
+		// show download
+		if (this.options.showDownload){
+		
+			this.viewer.adopt(new Element('div', { html: '<p><a href="#download">Download</a></p>' }).addClass('download'));
+			
+			self = this;
+			this.viewer.getElement('div.download a').addEvent('click', function(ev){
+				
+				if (ev) ev.stop();
+				
+				self.download();
+				
+			});
+		
+		}
+		
+		// show Zoom
+		if (this.options.showZoom){
+		
+			var self = this;
+			
+			this.viewer.adopt(new Element('div', {html: '<p></p>' }).addClass('zoom'));
+			
+			this.viewer.getElement('div.zoom p').adopt(new Element('select'));
+			
+			this.viewer.getElement('div.zoom select').adopt(new Element('option', { value: 0.25, html: '25%' }));
+			this.viewer.getElement('div.zoom select').adopt(new Element('option', { value: 0.50, html: '50%' }));
+			this.viewer.getElement('div.zoom select').adopt(new Element('option', { value: 0.75, html: '75%' }));
+			this.viewer.getElement('div.zoom select').adopt(new Element('option', { value: 1, html: '100%' }).set('selected', true));
+			this.viewer.getElement('div.zoom select').adopt(new Element('option', { value: 1.5, html: '150%' }));
+			this.viewer.getElement('div.zoom select').adopt(new Element('option', { value: 2, html: '200%' }));
+
+		}
+		
+		
+		// show thumblist?
+		if (this.options.showThumbs){
+		
+			var self = this;
+		
+			var tl = new Element('ul').addClass('thumblist').addEvent('click', function(ev){
+							
+				if (ev) ev.stop();
+				
+				var tar = document.id(ev.target);
+				
+				if (tar.get('tag') == 'img') tar = tar.getParent('a');
+				if (tar.get('tag') != 'a') return;
+			
+				self.changePage(parseInt(tar.getProperty('href').replace('#', '')) - 1);
+			
+			});
+		
+			this.viewer.adopt(tl);
+		}
+		
+		this.viewer.adopt(new Element('div', {html: '<p>Still to do:<br />* Canvas based for those that can<br />* Make zoom etc work<br />* Preloading<br />* Animations<br />* Drag/moving of main image</p>' }));
 	
 		// fire setup event
 		this.fireEvent('setup');
@@ -74,7 +178,8 @@ var InlinePDF = new Class({
 	// set the values of the pdf
 	success: function(j){
 		
-		if (j){
+		// error catching
+		if (!j){
 			this.failure();
 			return;
 		}
@@ -83,7 +188,7 @@ var InlinePDF = new Class({
 			this.failure(j.errormsg);
 			return;
 		}
-		
+				
 		// set values returned by server-side script
 		this.pagecount = j.pagecount;
 		this.pdfurl = j.pdfurl;
@@ -91,6 +196,18 @@ var InlinePDF = new Class({
 		this.pages = j.pages;
 		
 		// output pdf
+		if (this.pagecount > 0){
+				
+			// thumbs
+			this.viewer.getElement('ul.thumblist').empty();
+			this.thumbs.each(function(el, i){
+				this.viewer.getElement('ul.thumblist').adopt(new Element('li', { html: '<a href="#' + (i+1) + '"><img src="' + el + '" /></a>' }));
+			});
+			
+			// change page
+			this.changePage(0);
+		
+		}
 		
 		// fire show event
 		this.fireEvent('show');
@@ -114,14 +231,17 @@ var InlinePDF = new Class({
 		
 		// default options
 		var opts = $merge({
-			nocache: true
+			nocache: false
 		}, opts);
 		
-		// send request
-		this.request.send({
+		// make data to query string
+		var data = $H({
 			pdf: pdf,
 			options: JSON.encode(opts)
 		});
+		
+		// send request
+		this.request.send(data.toQueryString());
 		
 		return this;
 				
@@ -131,15 +251,25 @@ var InlinePDF = new Class({
 	changePage: function(page){
 	
 		if (this.pagecount > 0){
+				
 			if (this.pagecount >= page){
 			
 				// set currentpage var
 				this.currentpage = page;
+								
+				// output main image
+				this.viewer.getElement('div.page').set('html', '<img src="' + this.pages[page] + '" />');
+				
+				if (this.options.showThumbs){
 			
-				// need to add selected class to thumb
+					// remove selected from thumbs
+					this.viewer.getElements('ul.thumblist li').removeClass(this.options.selectedClass);
 				
-				// need to set the main image
+					// need to add selected class to thumb
+					this.viewer.getElements('ul.thumblist li')[page].addClass(this.options.selectedClass);
 				
+				}
+								
 				// need to update back/forward to selectable or not
 			
 			}
@@ -163,6 +293,26 @@ var InlinePDF = new Class({
 			this.changePage(this.currentpage + 1);
 		}
 		return this;
+	},
+	
+	// download file convenience function
+	download: function(){
+		window.open(this.pdfurl);
+	},
+	
+	// get
+	get: function(k){
+		try {
+			return eval('this.' + k);
+		} catch (e){
+			return null;
+		}
 	}
 
+});
+
+window.addEvent('domready', function(){
+	
+	window.ip = new InlinePDF({ el: document.id('viewer') }).show('http://www.nitec.com/cmsfiles/pdf/wg_2010_security_predictions.pdf');
+	
 });
--- Source/InlinePDF.php
+++ Source/InlinePDF.php
@@ -1,4 +1,5 @@
 <?
+
 /* 
 	
 	InlinePDF.php
@@ -18,26 +19,33 @@
 
 */
 
-$InlinePDF = new Class(
+ini_set('display_errors', 'off');
+
+class InlinePDF {
 
 	// private variables
 	private $pdf = '';
-	private $cachedir = './cache/';
+	private $cachedir = './cache';
 	private $cachedays = 30;
+	private $URL = 'http://pdf.rtnetworks.net/';
+	private $CACHEURL = 'http://pdf.rtnetworks.net/cache/';
 	
 	// constructor
 	public function __construct(){
-		
+					
 		// delete from cache where last accessed is over $cachedays
-		if ($dirlist = opendir($cachedir)){
+		if ($dirlist = opendir($this->cachedir)){
 			while (($file = readdir($dirlist)) !== false) {
-				if (is_dir($file)){
-					if (fileatime($cachedir.'/'.$file) <= (60*60*24*$cachedays)){
-						$this->recursiveDelete($cachedir.'/'.$file);
+				if (is_dir($this->cachedir.'/'.$file)){
+					if (fileatime($this->cachedir.'/'.$file) <= (60*60*24*$this->cachedays)){
+						//$this->recursiveDelete($this->cachedir.'/'.$file);
 					}
 				}
 			}
 		}
+		
+		// chaining
+		return $this;
 		
 	}
 	
@@ -54,62 +62,175 @@ $InlinePDF = new Class(
 			return @rmdir($str);
 		}
 	}
-	
+
 	// run function
-	public function run($pdf, $opts = object()){
+	public function run($pdf, $opts){
+
+		// catch objects
+		if (!isset($opts)) $opts = '';
 	
 		// get options
 		$opts = json_decode($opts);
 		
+		// set up an object
+		if (!$opts) $opts = (object) array();
+		
 		// default options
-		if ($opts == object()){
-			$opts->nocache = false;
-		}
+		if (!property_exists($opts, 'nocache')) $opts->nocache = false;
+		if (!property_exists($opts, 'pagemaxwidth')) $opts->pagemaxwidth = 1000;
+		if (!property_exists($opts, 'pagemaxheight')) $opts->pagemaxheight = 1000;
+		if (!property_exists($opts, 'thumbmaxwidth')) $opts->thumbmaxwidth = 100;
+		if (!property_exists($opts, 'thumbmaxheight')) $opts->thumbmaxheight = 75;
 	
 		// create a cache id
 		$cachemod = md5($pdf);
-		
+				
 		// does it exist in the cache, if so return it
-		if (!$opts->nocache && is_dir($cachedir.'/'.$cachemod.'/json.txt')){
-			return file_get_contents($cachedir.'/'.$cachemod.'/json.txt');
+		if (!$opts->nocache && file_exists($this->cachedir.'/'.$cachemod.'/json.txt')){
+			return json_decode(file_get_contents($this->cachedir.'/'.$cachemod.'/json.txt'));
 		}
-		
+				
 		try {
 		
 			// create our cache directory
-			mkdir($cachedir.'/'.$cachemod, '0777', true);
+			mkdir($this->cachedir.'/'.$cachemod, 0777, true);
+			
+			// filename
+			$filename = $this->cachedir.'/'.$cachemod.'/'.basename($pdf);
+			
+			// download the file
+			$bytes = file_put_contents($filename, file_get_contents($pdf));
+						
+			// make sure we downloaded something
+			if ($bytes === FALSE){
+				$json = array(
+					'error' => false,
+					'errormsg' => 'Could not download the PDF specified'
+				);
+				return $json;
+			}
 			
 			// json to return
 			$json = array(
 				'error' => false,
 				'pagecount' => 0,
-				'pdfurl' => $pdf,
+				'pdfurl' => $this->URL.$filename,
 				'thumbs' => array(),
 				'pages' => array()
 			);
+					
+			// set up imagick instance
+			$im = new Imagick();
 			
-			// use imagemagick to find out number of pages in pdf
+			// force resolution to screen
+			$im->setResolution(72,72);
 			
-			// use imagemagick to create a jpeg and thumb of each page, and add them to the corresponding arrays
+			// read image
+			$im->readImage($filename);
+			
+			// get image information
+			$imageInfo = $im->identifyImage();
+						
+			// ensure its a pdf!
+			if (!$imageInfo || strpos(strtolower($imageInfo['format']), 'pdf') === FALSE){
+				$json = array(
+					'error' => false,
+					'errormsg' => 'The file specified is not a PDF!'
+				);
+				return $json;
+			}
+			
+			// get geometry
+			$geometry = $imageInfo['geometry'];
+			
+			// resize ratio
+			$ratio = 1;
+			$thumbratio = 1;
+			
+			// landscape or portrait?
+			if ($geometry['width'] > $geometry['height']){
+				if ($geometry['width'] > $opts->pagemaxwidth){
+					$ratio = $opts->pagemaxwidth / $geometry['width'];
+				}	
+			} else {
+				if ($geometry['height'] > $opts->pagemaxheight){
+					$ratio = $opts->pagemaxheight / $geometry['height'];
+				}
+			}
+			
+			//  thumb ratio
+			if ($geometry['width'] > $geometry['height']){
+				if ($geometry['width'] > $opts->thumbmaxwidth){
+					$thumbratio = $opts->thumbmaxwidth / $geometry['width'];
+				}	
+			} else {
+				if ($geometry['height'] > $opts->thumbmaxheight){
+					$thumbratio = $opts->thumbmaxheight / $geometry['height'];
+				}
+			}
+			
+			// get the number of pages
+			$json['pagecount'] = $im->getNumberImages();
+						
+			// loop over pagecount
+			for ($i=0;$i<$json['pagecount'];$i++){
+						
+				// convert to jpg 
+				$im2 = new Imagick($filename.'['.$i.']');
+				//$im2->setImageColorspace(255); 
+				$im2->setCompression(Imagick::COMPRESSION_JPEG); 
+				$im2->setCompressionQuality(95); 
+				$im2->setImageFormat('jpeg'); 
+				
+				// main image max size
+				$im2->resizeImage($geometry['width'] * $ratio, $geometry['height'] * $ratio, imagick::FILTER_UNDEFINED, 1); 
+			
+				//write image on server 
+				$im2->writeImage($this->cachedir.'/'.$cachemod.'/page-'.$i.'.jpg'); 
+				
+				// thumb resize
+				$im2->resizeImage($geometry['width'] * $thumbratio, $geometry['height'] * $thumbratio, imagick::FILTER_UNDEFINED, 1); 
+				
+				//write image on server 
+				$im2->writeImage($this->cachedir.'/'.$cachemod.'/thumb-'.$i.'.jpg'); 
+				
+				// tidy up
+				$im2->clear();
+				$im2->destroy();
+				
+				// add to json
+				$json['thumbs'][] = $this->CACHEURL.$cachemod.'/thumb-'.$i.'.jpg';
+				$json['pages'][] = $this->CACHEURL.$cachemod.'/page-'.$i.'.jpg';
+			
+			}
+			
+			// tidy up
+			$im->clear(); 
+			$im->destroy(); 			
 			
 			// put json into file
-			file_put_contents($cachedir.'/'.$cachemod.'/json.txt', json_encode($json));
+			file_put_contents($this->cachedir.'/'.$cachemod.'/json.txt', json_encode($json));
 			
 			// return values
 			return $json;
 			
-		} catch (Exception $e){
-			return array(
+		} catch (exception $e){
+		
+			var_dump($e);
+				
+	 		return array(
 				'error' => true,
-				'errormsg' => $e
+				'errormsg' => $e->getMessage()
 			);
 		}
 	
 	}
 
-);
+};
 
-var $pdf = new InlinePDF();
+header('Content-Type: text/json');
+
+$pdf = new InlinePDF();
 echo json_encode($pdf->run($_POST['pdf'], $_POST['options']));
 
 ?>
